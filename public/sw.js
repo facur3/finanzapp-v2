7467bced05a6b3fbf84236fd9f581e778d68a75e
facur3 <facurossi33@gmail.com>
2026-07-01 11:02:46 -0300

    mejorar todo

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
    Infrastructure only: no UI, no push, no user-data caching. */
 'use strict';
 
-var CACHE = 'finanzapp-shell-v52';
+var CACHE = 'finanzapp-shell-v53';
 
 // App shell assets to precache. Kept intentionally small.
 var SHELL = [
